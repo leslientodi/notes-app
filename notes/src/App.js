e912bf82e123e6c3d5227e57d54be1911a377508
leslientodi <lesliehazard500@gmail.com>
2024-07-16 12:13:40 +0000

    different notes

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,21 +1,47 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "note title 1",
+      content: "content 1",
+    },
+    {
+      id: 2,
+      title: "note title 2",
+      content: "content 2",
+    },
+    {
+      id: 3,
+      title: "note title 3",
+      content: "content 3",
+    },
+    {
+      id: 4,
+      title: "note title 4",
+      content: "content 4",
+    },
+  ]);
+
   return (
     <div className="app-container">
       <form className="note-form">
         <input placeholder="title" required></input>
         <textarea placeholder="Content" rows={10} required></textarea>
-        <button type="submit"></button>
+        <button type="submit">Add Note</button>
       </form>
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
-        </div>
+        ))}
       </div>
     </div>
   );
