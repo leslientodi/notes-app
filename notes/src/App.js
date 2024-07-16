f264883d37a35dc4f068cddd3507d5ee9503cbf9
leslientodi <lesliehazard500@gmail.com>
2024-07-16 12:02:31 +0000

    added css

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,1 +1,22 @@
+import "./App.css";
 
+export default function App() {
+  return (
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="title" required></input>
+        <textarea placeholder="Content" rows={10} required></textarea>
+        <button type="submit"></button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+      </div>
+    </div>
+  );
+}
